c02fe97e2f1b86705e2aa8b99bcb857ed29ca2dd
questi0ns <rincondeloscuriosos@gmail.com>
2026-05-14 09:06:45 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // Si usas GitHub Pages, descomenta la línea de abajo
   // y pon el nombre exacto de tu repositorio:
-  // base: '/nombre-de-tu-repo/',
+  base: '/favicon-generator/',
   plugins: [react()],
 })
